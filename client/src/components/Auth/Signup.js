7516717e510a1318ddf4e765c55c898d64818b0b
ngovanhuong94 <ngovanhuong21994@gmail.com>
2019-03-09 17:21:08 +0300

    add style register page

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Signup = () => (
-	<div>
-		This is Signup page
-	</div>
-)
+class Signup extends Component {
+	render () {
+		return (
+			<div className="content">
+				<h2>Register</h2>
+				<form className="form">
+					<input 
+						type="text"
+						placeholder="Username"
+					/>
+					<input 
+						type="text"
+						placeholder="Email"
+					/>
+					<input 
+						type="password"
+						placeholder="Password"
+					/>
+					<input 
+						type="password2"
+						placeholder="Repeat password"
+					/>
+					<button>
+						Submit
+					</button>
+				</form>
+			</div>
+		)
+	}
+}
 
 export default Signup
