be87d8e18ca18cfb6b27ea1928a8af8907b88bea
ngovanhuong94 <ngovanhuong21994@gmail.com>
2019-03-09 12:21:06 +0300

    client setup Home and Navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => (
+	<div>
+		navbar component
+	</div>
+)
+
+export default Navbar
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from './components/Home';
+import Navbar from './components/Navbar'
+
+import * as serviceWorker from './serviceWorker';
+const Root = () => (
+	<BrowserRouter>
+		<Fragment>
+			<Navbar />
+			<Switch>
+				<Route exact path="/" component={Home} />
+			</Switch>
+		</Fragment>
+	</BrowserRouter>
+)
+
+
+
+
+
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
